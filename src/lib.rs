5614b14ad90f41542cd3212230fb83016c2ada5f
Seth Bashford <seth.bashford@boystown.org>
2024-06-13 15:41:40 -0500

    add anyhow, get rid of custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use anyhow::{anyhow, Context};
+
 type Microseconds = i64;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -48,65 +50,28 @@ pub struct Behavior {
     pub accuracy_percentage: f32,
 }
 
-#[derive(Debug)]
-pub struct ContextualError {
-    pub context: Vec<String>,
-    pub detailed: Option<String>,
-    what: &'static str,
-}
-
-impl From<&'static str> for ContextualError {
-    fn from(what: &'static str) -> Self {
-        ContextualError {
-            context: vec![],
-            detailed: None,
-            what,
-        }
-    }
-}
-
-impl std::error::Error for ContextualError {}
-
-impl std::fmt::Display for ContextualError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for c in self.context.iter().rev() {
-            write!(f, "{c}: ")?;
-        }
-        if let Some(detail) = &self.detailed {
-            write!(f, "{}: {}", self.what, detail)
-        } else {
-            write!(f, "{}", self.what)
-        }
-    }
-}
-
-pub fn parse_triggers(input: &str) -> Result<Vec<Trigger>, ContextualError> {
+pub fn parse_triggers(input: &str) -> anyhow::Result<Vec<Trigger>> {
     input
         .lines()
         .enumerate()
         .filter(|(_, line)| line.contains("FIFF Trigger"))
-        .map(|(i, line)| {
-            parse_trigger_line(line).map_err(|mut e| {
-                e.context.push(format!("on line {i}"));
-                e
-            })
-        })
+        .map(|(i, line)| parse_trigger_line(line).with_context(|| format!("on line {i}")))
         .collect()
 }
 
-fn parse_trigger_line(line: &str) -> Result<Trigger, ContextualError> {
+fn parse_trigger_line(line: &str) -> anyhow::Result<Trigger> {
     let mut tokens = line.trim().split_whitespace();
     let time_microseconds = tokens
         .next()
-        .ok_or("missing time (Tmu)")?
+        .ok_or(anyhow!("missing time (Tmu)"))?
         .parse::<i64>()
-        .map_err(|_| "unable to parse time (Tmu) as integer")?;
+        .map_err(|_| anyhow!("unable to parse time (Tmu) as integer"))?;
     tokens.next();
     let code = tokens
         .next()
-        .ok_or("missing trigger number (TriNo)")?
+        .ok_or(anyhow!("missing trigger number (TriNo)"))?
         .parse::<i32>()
-        .map_err(|_| "unable to parse trigger number (TriNo) as integer")?;
+        .map_err(|_| anyhow!("unable to parse trigger number (TriNo) as integer"))?;
     Ok(Trigger {
         time_microseconds,
         code,
@@ -308,7 +273,7 @@ Someone put something unexpected on this line
         );
         assert_eq!(
             "on line 2: unable to parse trigger number (TriNo) as integer",
-            format!("{}", triggers.err().unwrap())
+            format!("{:#}", triggers.err().unwrap())
         );
     }
 
